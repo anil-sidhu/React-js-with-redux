3999f8cd5091af3f9cf6b02089547dcfcd98f93e
anil <anil.kumar1@kelltontech.com>
2018-06-18 15:14:16 +0530

    Add newtork graph and vaildation

--- src/Components/LoginComponent.js
+++ src/Components/LoginComponent.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 import {
     Button, Alert, Navbar, MenuItem, NavDropdown, Nav, NavItem, Header, SplitButton, Modal,
 } from 'react-bootstrap';
-// import Button from 'react-bootstrap/lib/Button';
 import { BrowserRouter as Router, Route, Link, } from "react-router-dom";
-
 import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
-
-
+import Calendar, { formatDate } from 'react-calendar';
 class Login extends Component {
-
     constructor(props) {
         super(props);
-
-
         this.state = {
             show: false,
             email: '',
             password: '',
             account: true,
             name: '',
-            dob:'',
-            nameError:''
+            dob: "",
+            showCalender: false,
+            nameError: '',
+            emailError: ''
         };
+    }
+    signUp() {
+        alert("done")
     }
     login() {
         let loginObj = {};
@@ -34,44 +33,51 @@ class Login extends Component {
         this.props.login(4)
     }
     valid(item, type) {
-
         let itemValue = item.target.value;
-
-
         switch (type) {
             case "name":
-               {
-                   if(itemValue.length<6)
-                    {
-                        item.target.style.color="red";
-                        this.state.nameError=<span style={{color:'red'}}>Error Message </span>;
+                {
+                    if (itemValue.length < 2) {
+                        item.target.style.color = "red";
+                        this.state.nameError = <span style={{ color: 'red' }}>Error Message </span>;
                     }
-                    else
-                        {
-                            item.target.style.color="";
-                            this.state.nameError="";
-                        }
-                this.setState({ name: itemValue });
-                break;
-               }
+                    else {
+                        item.target.style.color = "";
+                        this.state.nameError = "";
+                    }
+                    this.setState({ name: itemValue });
+                    break;
+                }
             case "email":
-                this.setState({ email: itemValue });
-                break;
-            case "dob":
-                this.setState({ dob: itemValue });
+                let errorMsg
+                var emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                let validation = emailValidation.test(String(itemValue).toLowerCase());
+                if (!validation) {
+                    errorMsg = "Not valid"
+                    item.target.style.color = "red";
+                    this.state.emailError = <span style={{ color: 'red' }}>Error Message </span>;
+                }
+                else {
+                    item.target.style.color = "";
+                    this.state.emailError = "";
+                }
+                this.setState({ email: itemValue, });
                 break;
             case "password":
-                this.setState({ password: itemValue });
+               { this.setState({ password: itemValue });}
                 break;
-               
-        }
-        console.warn("valid obj", itemValue.length)
-        
-        
-        
-        
-    }
 
+        }
+    }
+    onChangeDob(selectedDate) {
+        var month = selectedDate.getMonth();
+        var year = selectedDate.getFullYear();
+        var date = selectedDate.getDate();
+        this.setState({
+            dob: date + "/" + month + "/" + year,
+            showCalender: false
+        })
+    }
     render() {
         return (
             <div>
@@ -80,7 +86,6 @@ class Login extends Component {
                         <Modal.Title>Login/Sign up</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-
                         {
                             this.state.account ?
                                 <div>
@@ -111,37 +116,44 @@ class Login extends Component {
 
                                             <label>
                                                 Name* {this.state.nameError}
-                                                      <Input value={this.state.name} className="form-control " name='name' onChange={(text) => { this.valid(text, "name") }} />
+                                                <Input value={this.state.name} className="form-control " name='name' onChange={(text) => { this.valid(text, "name") }} />
                                             </label>
                                         </div>
                                         <div>
 
                                             <label>
-                                                Email*
-                                                      <Input value={this.state.email} className="form-control" name='email' onChange={(text) => { this.valid(text, "email") }} />
+                                                Email* {this.state.emailError}
+                                                <Input value={this.state.email} className="form-control" name='email' onChange={(text) => { this.valid(text, "email") }} />
                                             </label>
                                         </div>
                                         <div>
+                                            {
+                                                this.state.showCalender ?
+                                                    <Calendar
+                                                        maxDate={new Date("2014-05-11")}
+                                                        onChange={this.onChangeDob.bind(this)}
 
+                                                    />
+                                                    : null
+                                            }
                                             <label>
                                                 DOB
-                                                      <Input value={this.state.dob} className="form-control" name='email' onChange={(text) => { this.valid(text,"dob") }} />
+                                                <Input onClick={() => this.setState({ showCalender: true })} type="text" value={this.state.dob} className="form-control" name='set' />
                                             </label>
                                         </div>
                                         <div>
                                             <label>
                                                 Password*
-                                                      <Input type='password' name='password' value={this.state.password} className="form-control" onChange={(text) => { this.valid(text,"password") }} />
+                                                      <Input type='password' name='password' value={this.state.password} className="form-control" onChange={(text) => { this.valid(text, "password") }} />
                                             </label>
                                         </div>
                                         <div>
-                                            <Button onClick={() => { this.login() }} className="btn btn-primary">Sign up</Button>
+                                            <Button onClick={() => { this.signUp() }} className="btn btn-primary">Sign up</Button>
                                             <Button onClick={() => this.props.hideModel()}>Close</Button>
                                         </div>
                                     </Form>
                                     <a className="btn" onClick={() => { this.setState({ account: true }) }}  >Login</a>
                                 </div>
-
                         }
                     </Modal.Body>
                 </Modal>
@@ -149,6 +161,5 @@ class Login extends Component {
         );
     }
 }
-
 
 export default Login; 
--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import { Provider } from 'react-redux'
 import { createStore,applyMiddleware } from 'redux'
 import rootReducer from './rootReducer'
 import ReduxThunk from 'redux-thunk';
-// import App from './Containers/AppContainer';
-import App from './Components/UploadComponent'
+import App from './Containers/AppContainer';
 
 const store = createStore(rootReducer,applyMiddleware(ReduxThunk))
 ReactDOM.render(<Provider store={store}>
